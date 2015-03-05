e5beb74714548835f711b32e00b44a1d588e2c16
Rohan <rlikhite@gmail.com>
2015-03-05 15:14:16 -0500

    v1.0

--- js/magic.js
+++ js/magic.js
@@ -1,8 +1,11 @@
+//Rohan Likhite
+
 // Acceleration
 var ax = 0;
 var ay = 0;
 var az = 0;
 
+// Timer
 var clicked = false;
 var sec = 0;
 var min = 0;
@@ -12,6 +15,7 @@ var breakTime = false;
 var resume = false;
 var lastPom = 0;
 var minDisplay = 0;
+var clock;
 
 
 
@@ -27,29 +31,37 @@ function startClock() {
 function stopWatch() {
     lastPom = pomodoros;
     if (pomodoros > 0 && breakTime) {
-        document.body.style.backgroundColor = "#04BF55";
-        
+        document.body.style.backgroundColor = "#2ECC71";
+        document.getElementById("labelIcon").src = "images/success.svg";
+        document.getElementById("labelIcon").className += "symbol";
+        document.getElementById("bottomTag").innerHTML = "Take a 5 min break";
+
     }
-    min = Math.floor(sec / 3);
+    if (!breakTime) {
+        document.getElementById("labelIcon").src = "images/logo.svg";
+        document.getElementById("labelIcon").className = "symbol fa-spin";
+        document.getElementById("bottomTag").innerHTML = "25 min = 1 Pomodoro";
+    }
+    min = Math.floor(sec / 10);
     pomodoros = Math.floor(min / pomMins);
     if (pomodoros - lastPom == 1) {
         breakTime = true;
     }
     minDisplay = min % pomMins;
     sec++;
-    document.getElementById("sec").innerHTML = sec;
+    console.log(sec);
     document.getElementById("timer").innerHTML = minDisplay;
     document.getElementById("pomNum").innerHTML = pomodoros;
 
 }
 
 function stopClock() {
-    alert(resume);
-    if(!resume){
-    sec = 0;
-    pomodoros = 0;
-    min = 0; 
-    window.clearInterval(clock);
+    //alert(resume);
+    if (!resume) {
+        sec = 0;
+        pomodoros = 0;
+        min = 0;
+        window.clearInterval(clock);
     }
     window.clearInterval(clock);
     pomodoros = lastPom;
@@ -59,11 +71,13 @@ function stopClock() {
 }
 
 function pauseClock() {
-    alert("on pause");
+    //alert("on pause");
+
     clicked = false;
     breakTime = false;
     lastPom = pomodoros;
     resume = true;
+
 
 
 }
@@ -82,20 +96,32 @@ if (window.DeviceMotionEvent == undefined) {
 
         if (ax !== 0 | ay !== 0) {
             if (breakTime) {
-                pauseClock()
+                pauseClock();
+                stopClock();
             } else if (!breakTime) {
                 stopClock()
+                document.getElementById("bottomTag").innerHTML = "A Pomodoro Timer";
+                document.body.style.backgroundColor = "#E26A6A";
             }
+            if (lastPom > 0) {
+                document.getElementById("line1").innerHTML = "You can take a 5 min break";
+                document.getElementById("total").innerHTML = lastPom + "<br/> Pomodoros Completed";
+                document.body.style.backgroundColor = "#2ECC71";
+            }
+
             document.getElementById("leveled").style.display = "none";
             document.getElementById("notLeveled").style.display = "block";
-            document.body.style.backgroundColor = "#D9304F ";
+
+
+
 
         } else {
             startClock()
             document.getElementById("notLeveled").style.display = "none";
             document.getElementById("leveled").style.display = "block";
             if (!breakTime) {
-                document.body.style.backgroundColor = "#F27B50";
+                document.body.style.backgroundColor = "#E26A6A";
+                document.getElementById("bottomTag").innerHTML = "25 min = 1 Pomodoro";
             }
 
 
