6c8ba81a2745e5cfe506bda8f37cbff5564b909e
Rohan <rlikhite@gmail.com>
2015-03-05 13:33:24 -0500

    it works

--- js/magic.js
+++ js/magic.js
@@ -7,37 +7,65 @@ var clicked = false;
 var sec = 0;
 var min = 0;
 var pomodoros = 0;
-var time = 10;
+var pomMins = 2;
+var breakTime = false;
+var resume = false;
+var lastPom = 0;
+var minDisplay = 0;
+
+
 
 function startClock() {
     if (clicked === false) {
         clock = setInterval("stopWatch()", 1000);
         clicked = true;
-    }
-    else if (clicked === true) {
-    }
+    } else if (clicked === true) {}
+
+
 }
 
 function stopWatch() {
-    min = Math.floor(sec / 60);
-    pomodoros = Math.floor(min / 25);
-    sec++; 
+    lastPom = pomodoros;
+    if (pomodoros > 0 && breakTime) {
+        document.body.style.backgroundColor = "#04BF55";
+        
+    }
+    min = Math.floor(sec / 3);
+    pomodoros = Math.floor(min / pomMins);
+    if (pomodoros - lastPom == 1) {
+        breakTime = true;
+    }
+    minDisplay = min % pomMins;
+    sec++;
     document.getElementById("sec").innerHTML = sec;
-    document.getElementById("timer").innerHTML = min;
+    document.getElementById("timer").innerHTML = minDisplay;
     document.getElementById("pomNum").innerHTML = pomodoros;
-    
+
 }
 
 function stopClock() {
-    window.clearInterval(clock);
+    alert(resume);
+    if(!resume){
     sec = 0;
     pomodoros = 0;
-    min = 0;
-    document.getElementById("timer").innerHTML="";
-   
+    min = 0; 
+    window.clearInterval(clock);
+    }
+    window.clearInterval(clock);
+    pomodoros = lastPom;
+    document.getElementById("timer").innerHTML = "";
     document.getElementById("pomNum").innerHTML = "";
-    
     clicked = false;
+}
+
+function pauseClock() {
+    alert("on pause");
+    clicked = false;
+    breakTime = false;
+    lastPom = pomodoros;
+    resume = true;
+
+
 }
 
 
@@ -45,7 +73,7 @@ if (window.DeviceMotionEvent == undefined) {
     document.getElementById("no").style.display = "block";
     document.getElementById("yes").style.display = "none";
 } else {
-    
+
     window.ondevicemotion = function (event) {
         ax = Math.round(Math.abs(event.accelerationIncludingGravity.x * 1));
         ay = Math.round(Math.abs(event.accelerationIncludingGravity.y * 1));
@@ -53,7 +81,11 @@ if (window.DeviceMotionEvent == undefined) {
         ai = Math.round(event.interval * 100) / 100;
 
         if (ax !== 0 | ay !== 0) {
-            stopClock() 
+            if (breakTime) {
+                pauseClock()
+            } else if (!breakTime) {
+                stopClock()
+            }
             document.getElementById("leveled").style.display = "none";
             document.getElementById("notLeveled").style.display = "block";
             document.body.style.backgroundColor = "#D9304F ";
@@ -62,13 +94,14 @@ if (window.DeviceMotionEvent == undefined) {
             startClock()
             document.getElementById("notLeveled").style.display = "none";
             document.getElementById("leveled").style.display = "block";
-            document.body.style.backgroundColor = "#F27B50";
+            if (!breakTime) {
+                document.body.style.backgroundColor = "#F27B50";
+            }
 
 
         }
 
     }
 
-   
-}
 
+}
